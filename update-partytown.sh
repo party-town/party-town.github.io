49e7be48513ed57edd0eab219964c18d734a58d5
Lionel Faber <lionel1704@gmail.com>
2020-05-17 08:33:12 +0530

    initial script

---
+++ update-partytown.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent https://partytown.pythonanywhere.com
+wget https://partytown.pythonanywhere.com/static/api/images/service-gallerRight-arrow.png -P partytown.pythonanywhere.com/static/api/images/
+wget https://partytown.pythonanywhere.com/static/api/images/service-gallerLeft-arrow.png -P partytown.pythonanywhere.com/static/api/images/
