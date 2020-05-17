2fddbfafc3a8fad712fd375e380143e4d1b14230
Lionel Faber <lionel1704@gmail.com>
2020-05-17 13:10:44 +0530

    Update clone-site.sh

--- clone-site.sh
+++ clone-site.sh
@@ -2,4 +2,5 @@
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent https://partytown.pythonanywhere.com
 wget https://partytown.pythonanywhere.com/static/api/images/service-gallerRight-arrow.png -P partytown.pythonanywhere.com/static/api/images/
 wget https://partytown.pythonanywhere.com/static/api/images/service-gallerLeft-arrow.png -P partytown.pythonanywhere.com/static/api/images/
+wget https://partytown.pythonanywhere.com/sitemap.xml -P partytown.pythonanywhere.com/
 echo "partytown.co.in" > partytown.pythonanywhere.com/CNAME
